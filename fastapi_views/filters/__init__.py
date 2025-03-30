2329ec4525908638a7f3018f66fd819f76c4ee23
RaRhAeu <37556570+RaRhAeu@users.noreply.github.com>
2025-03-30 19:05:20 +0200

    feat: filters reexport (#35)

---
+++ fastapi_views/filters/__init__.py
@@ -0,0 +1,20 @@
+from .dependencies import FilterDepends, NestedFilter
+from .models import (
+    AnyFilter,
+    BaseFilter,
+    Filter,
+    ModelFilter,
+    OrderingFilter,
+    SearchFilter,
+)
+
+__all__ = [
+    "AnyFilter",
+    "BaseFilter",
+    "Filter",
+    "FilterDepends",
+    "ModelFilter",
+    "NestedFilter",
+    "OrderingFilter",
+    "SearchFilter",
+]
